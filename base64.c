87036c8ab41e93b80facc19feb434cac76f3d15f
Sekaii22 <shawn.ng22@gmail.com>
2025-04-25 02:10:39 +0800

    Add function to get decoded length

--- base64.c
+++ base64.c
@@ -5,9 +5,9 @@
 char dictionary[64 + 1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 /* 
-    Calculate the final base64 length needed to encode the text data, msg. 
+    Calculate the base64 length after encoding the message. 
 */
-int getBase64Len(char *msg) {
+int getBase64EncodeLen(char *msg) {
     int msgBitLen = strlen(msg) * 8;
 
     // no of base64 char needed to encode msg
@@ -18,6 +18,13 @@ int getBase64Len(char *msg) {
     int finalLen = ((intialLen + (4 - 1)) / 4) * 4;               // always round up
 
     return finalLen;
+}
+
+/*
+    Calculate the message length after decoding the base64 encoding.
+*/
+int getBase64DecodeLen(char *encoding) {
+    return (strlen(encoding) * 6) / 8; 
 }
 
 /*
@@ -37,7 +44,7 @@ int getIndexFromStr(char *str, char *subStr) {
 void encode(char *msg, char *output) {
     int bitIndex = 0;
     int msgBitLen = strlen(msg) * 8;
-    int base64Len = getBase64Len(msg);
+    int base64Len = getBase64EncodeLen(msg);
     int currLen = 0;
     char base64Str[base64Len + 1];
 
@@ -81,43 +88,13 @@ void encode(char *msg, char *output) {
     Decodes base64 encoding to a UTF-8 string. Message is stored in output.
 */
 void decode(char *encoding, char *output) {
-    // four characters are typically converted back to three bytes
+    // four base64 characters are converted back to three bytes
     int encodingLen = strlen(encoding);
     int encodingIndex = 0;
 
-    char msg[200];
+    char msg[getBase64DecodeLen(encoding) + 1];
     int msgIndex = 0;
 
-    // while (encodingIndex + 4 <= encodingLen) {
-    //     char code[4 + 1] = {0};
-    //     unsigned int value = 0;
-    //     int valBitLen = 0;
-
-    //     // read 4 characters
-    //     strncpy(code, encoding + encodingIndex, 4);
-    //     encodingIndex += 4;
-
-    //     for (int i = 0; i < 4; i++) {
-    //         if (code[i] == '=') {
-                
-    //             continue;
-    //         }
-
-    //         // Use table to translate base64 char to int
-    //         char search[] = {code[i], '\0'};
-    //         int pos = getIndexFromStr(dictionary, search);
-
-    //         for (int j = 0; j < 6; j++) {
-
-    //         }
-            
-
-    //         // get the decimal vals from the 4 chars
-
-    //         // bitwise shift left by 2
-
-    //     }
-    // }
     unsigned int value = 0;
     int valueIndex = 0;
 
@@ -159,12 +136,12 @@ void decode(char *encoding, char *output) {
 int main(int argc, char *arcv) {
     
     // char msg[] = "Many hands make light work.";
-    // char encoding[getBase64Len(msg) + 1];
+    // char encoding[getBase64EncodeLen(msg) + 1];
     // encode(msg, encoding);
     // printf("%s\n", encoding);
 
-    char encoding[] = "bGlnaHQgdw==";
-    char msg[100];
+    char encoding[] = "TWFueSBoYW5kcyBtYWtlIGxpZ2h0IHdvcmsu";
+    char msg[getBase64DecodeLen(encoding) + 1];
     decode(encoding, msg);
     printf("%s\n", msg);
 
